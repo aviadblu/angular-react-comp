a4eaeb403ccf9906496db24a65730ed08860c5d0
Aviad Blumenfeld <aviadblu@gmail.com>
2016-01-21 11:17:46 +0200

    remove test

--- src/app/components/hello.js
+++
@@ -1,8 +0,0 @@
-
-var HelloMessage = React.createClass({displayName: "HelloMessage",
-    render: function() {
-        return React.createElement("div", null, "Hello ", this.props.name);
-    }
-});
-
-ReactDOM.render(React.createElement(HelloMessage, {name: "John"}), document.getElementById('targetNode'));
